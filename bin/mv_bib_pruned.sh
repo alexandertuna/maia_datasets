0c6287b2e8d6e16173fd34ee56eb61ead2de2897
alexandertuna <alexander.tuna@gmail.com>
2026-08-16 15:20:03 -0700

    Some wrapper scripts for making datasets

---
+++ bin/mv_bib_pruned.sh
@@ -0,0 +1,9 @@
+TOTAL=6666
+
+mkdir -p BIB10TeV/sim_mm_pruned
+mkdir -p BIB10TeV/sim_mp_pruned
+for NUM in $(seq 1 ${TOTAL}); do
+    for MUON in mm mp; do
+        mv ${MUON}_pruned_BIB_sim_${NUM}.slcio BIB10TeV/sim_${MUON}_pruned/BIB_sim_${NUM}.slcio
+    done
+done
