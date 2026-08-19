f797ffb834dd69ed92fbe06a0191d7b7937d74bd
alexandertuna <alexander.tuna@gmail.com>
2026-08-18 20:45:46 -0700

    Document my suffering

--- productions/ttbar.2026_08_17_21h00m00s/postprocess_one_file.sh
+++ productions/ttbar.2026_08_17_21h00m00s/postprocess_one_file.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# env
+# it would be cool if setup_mucoll existed out-of-the-box
+# setup_mucoll
+# source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-master-2wtmg3ohr26uckseodhqjfjaw7mijwil/setup.sh
+
+# preamble
+echo "SHELL=$SHELL"
+echo "PWD=$PWD"
+echo "HOST=$(hostname)"
+which python || true
+python --version || true
+python3.10 --version || true
+python3.11 --version || true
+python3.12 --version || true
+
 #
 # ./postprocess_one_file.sh 10000
 #
@@ -16,7 +31,22 @@ echo "NUM=${NUM}"
 echo "INPUT=${INPUT}"
 echo "OUTPATH=${OUTPATH}"
 
-source ${PFLOW}/.venv/bin/activate
+# env
+python -m venv ./env
+source env/bin/activate
+python -m pip install awkward fastjet numpy tqdm uproot vector scipy pydantic comet_ml pyyaml pyarrow
+
+# particleflow
+export PYTHONPATH=/ceph/users/atuna/work/maia/mlpf_postprocess/particleflow
+echo "PYTHONPATH"
+echo "$PYTHONPATH"
+ls $PYTHONPATH
+# git clone --recurse-submodules https://github.com/jpata/particleflow.git
+# python -m pip install --no-deps particleflow
+# python -m pip install --no-deps /ceph/users/atuna/work/maia/mlpf_postprocess/particleflow_containered
+
+# source ${PFLOW}/.venv/bin/activate
 # which python
-# uv run python ${PFLOW}/mlpf/data/key4hep/postprocessing.py --input ${INPUT} --outpath ${OUTPATH} --detector maia
+# python --version
 python ${PFLOW}/mlpf/data/key4hep/postprocessing.py --input ${INPUT} --outpath ${OUTPATH} --detector maia
+rm -rf ./env ./particleflow
