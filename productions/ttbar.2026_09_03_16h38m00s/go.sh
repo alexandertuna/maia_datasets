b5ac975dee70b39c715c3673cb705780a98a6598
alexandertuna <alexander.tuna@gmail.com>
2026-09-03 17:36:54 -0700

    Add reco and postprocess too

--- productions/ttbar.2026_09_03_16h38m00s/go.sh
+++ productions/ttbar.2026_09_03_16h38m00s/go.sh
@@ -4,6 +4,8 @@ set -eo pipefail
 #
 # apptainer run /cvmfs/unpacked.cern.ch/ghcr.io/muoncollidersoft/mucoll-sim-alma9:v2.9.8-amd64
 # setup_mucoll
+# CODE=/ceph/users/atuna/work/maia
+# export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
 #
 
 #
@@ -18,36 +20,67 @@ EVENTS_PER_JOB=10
 #
 CODE=/ceph/users/atuna/work/maia
 COMPACT=${CODE}/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml
+PFLOW=${CODE}/maia/mlpf_postprocess/particleflow
 
 #
 # file names
 #
+TYPEEVENT="ttbar"
 GEN_CMD=p8_mumu_tt_ecm10000_${SEED}.cmd
-GEN_HEPMC=ttbar_${SEED}.hepmc
+GEN_HEPMC=${TYPEEVENT}_${SEED}.hepmc
 SIM_STEER=${TOPDIR}/sim_steer_GEN_CONDOR.py
-SIM_SLCIO=ttbar_sim_${SEED}.slcio
+SIM_SLCIO=${TYPEEVENT}_sim_${SEED}.slcio
+REC_STEER=${TOPDIR}/steer_reco_${TYPEEVENT}.py
+REC_SLCIO=${TYPEEVENT}_reco_${SEED}.slcio
+REC_ROOT=${REC_SLCIO}.edm4hep.root
+REC_PATCH=${TOPDIR}/patch.txt
+GEN_LOG=log_gen_${SEED}.txt
+SIM_LOG=log_sim_${SEED}.txt
+REC_LOG=log_rec_${SEED}.txt
+POST_LOG=log_post_${SEED}.txt
 
 #
 # gen
 #
+echo "Running gen ${SEED} ..."
 cp ${TOPDIR}/p8_mumu_tt_ecm10000.cmd ${GEN_CMD}
 sed -i s/12345/${SEED}/g ${GEN_CMD}
-time k4run pythia.py --Dumper.Filename ${GEN_HEPMC} --Pythia8.PythiaInterface.pythiacard ${GEN_CMD}
+k4run \
+    pythia.py \
+    --Dumper.Filename ${GEN_HEPMC} \
+    --Pythia8.PythiaInterface.pythiacard ${GEN_CMD} \
+    &> ${GEN_LOG}
 
 #
 # sim
 #
-time ddsim \
-     --inputFile ${GEN_HEPMC} \
-     --steeringFile ${SIM_STEER} \
-     --compactFile ${COMPACT} \
-     --numberOfEvents ${EVENTS_PER_JOB} \
-     --outputFile ${SIM_SLCIO}
+echo "Running sim ${SEED} ..."
+ddsim \
+    --inputFile ${GEN_HEPMC} \
+    --steeringFile ${SIM_STEER} \
+    --compactFile ${COMPACT} \
+    --numberOfEvents ${EVENTS_PER_JOB} \
+    --outputFile ${SIM_SLCIO} \
+    &> ${SIM_LOG}
 
 #
-# reco
+# rec
 #
+echo "Running rec ${SEED} ..."
+# k4run ${REC_STEER} \
+#      --TypeEvent ${TYPEEVENT} \
+#      --InFileName ${SEED} \
+#      --code ${CODE} \
+#      --skipTrackerConing \
+#      &> ${REC_LOG}
+# lcio2edm4hep ${REC_SLCIO} ${REC_ROOT} ${REC_PATCH}
 
 #
 # postprocess
 #
+# python \
+#     ${PFLOW}/mlpf/data/key4hep/postprocessing.py \
+#     --input ${REC_ROOT} \
+#     --outpath $(pwd) \
+#     --detector maia \
+#     &> ${POST_LOG}
