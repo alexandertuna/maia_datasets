3e29c37fef4738c23c69afea8313b4011cf71422
alexandertuna <alexander.tuna@gmail.com>
2026-09-14 17:42:03 -0700

    Add a wildcard so the setup works for alma9 and ubuntu24

--- productions/bib.2026_08_14_17h50m00s/simulate_bib.sh
+++ productions/bib.2026_08_14_17h50m00s/simulate_bib.sh
@@ -24,7 +24,7 @@ echo "COMPACT=${COMPACT}"
 # env
 # it would be cool if setup_mucoll existed out-of-the-box
 # setup_mucoll
-source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-master-2wtmg3ohr26uckseodhqjfjaw7mijwil/setup.sh
+source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-*/setup.sh
 export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
 
 # process
