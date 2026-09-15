b145c028ee91d0403f82f4f6bc896bde9779b2c2
alexandertuna <alexander.tuna@gmail.com>
2026-09-14 18:15:23 -0700

    Messing with 20um resolution too

--- productions/neutrinoGun.2026_08_16_18h43m00s/digitize_neutrinos.sh
+++ productions/neutrinoGun.2026_08_16_18h43m00s/digitize_neutrinos.sh
@@ -13,12 +13,12 @@ TYPEEVENT="neutrinoGun"
 # export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
 
 # for RESOLUTIONUV in 0.000 0.005 0.010 0.020 0.050; do
-for RESOLUTIONUV in 0.000 0.020 0.050; do
+for RESOLUTIONUV in 0.020; do
 
     mkdir -p ${RESOLUTIONUV}
     cd ${RESOLUTIONUV}
 
-    for NUM in $(seq 20 49); do
+    for NUM in $(seq 0 9); do
 
         # run
         echo "Running ${RESOLUTIONUV} ${NUM} ..."
@@ -31,9 +31,6 @@ for RESOLUTIONUV in 0.000 0.020 0.050; do
              --ResolutionUV ${RESOLUTIONUV} \
              --data ${DATA} \
              --typeevent ${TYPEEVENT} &> neutrinoGun_log_${NUM}.txt
-
-        # echo "Moving ..."
-        # mv neutrinoGun_*  ${RESOLUTIONUV}/
 
     done
 
