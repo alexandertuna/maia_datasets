66edc936a0d6cc350a5cee96176d0d64314ec17d
alexandertuna <alexander.tuna@gmail.com>
2026-09-15 16:54:46 -0700

    Make v07 muons, and wildcard

--- productions/muonGun.2026_09_13_18h29m00s/run_muons.sh
+++ productions/muonGun.2026_09_13_18h29m00s/run_muons.sh
@@ -4,12 +4,12 @@ set -eo pipefail
 MAX_JOBS=10
 CODE=/ceph/users/atuna/work/maia
 TYPEEVENT="muonGun_pT_2p0_2p1"
-DETECTOR="v06"
+DETECTOR="v07"
 
 # env
 # it would be cool if setup_mucoll existed out-of-the-box
 # setup_mucoll
-# source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-master-2wtmg3ohr26uckseodhqjfjaw7mijwil/setup.sh
+# source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-*/setup.sh
 # export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
 
 # for RESOLUTIONUV in 0.000 0.005 0.010 0.020; do
@@ -19,7 +19,7 @@ for RESOLUTIONUV in 0.020; do
     mkdir -p ${DETECTOR}/${RESOLUTIONUV}
     cd ${DETECTOR}/${RESOLUTIONUV}
 
-    for NUM in {310..399}; do
+    for NUM in {300..399}; do
 
         while (( $(jobs -rp | wc -l) >= MAX_JOBS )); do
             echo "Waiting at $(date) ..."
