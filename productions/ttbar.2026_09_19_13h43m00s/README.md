472704d2294339d768bf6e5f024608c63fd0e5ab
alexandertuna <alexander.tuna@gmail.com>
2026-09-19 13:50:17 -0700

    A small ttbar dataset for visualizing a muon collisioin

---
+++ productions/ttbar.2026_09_19_13h43m00s/README.md
@@ -0,0 +1,1 @@
+Making small ttbar dataset from start (pythia8 card) to sim MCParticles for visualization only
---
+++ productions/ttbar.2026_09_19_13h43m00s/go.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+#
+# activate container like this when running locally
+# apptainer run /cvmfs/unpacked.cern.ch/ghcr.io/muoncollidersoft/mucoll-sim-ubuntu24:v2.9.8-amd64
+# setup_mucoll
+#
+
+#
+# command line args
+#
+if [ -z "$1" ]; then
+    echo "Usage: $0 SEED"
+    exit 1
+fi
+SEED=${1}
+TOPDIR=/ceph/users/atuna/work/maia/maia_datasets/productions/ttbar.2026_09_19_13h43m00s
+EVENTS_PER_JOB=2 # 10
+
+#
+# steering
+#
+DO_GEN=true
+DO_SIM=true
+DO_REC=false
+DO_POST=false
+DO_CLEANUP=false
+
+#
+# paths
+#
+CODE=/ceph/users/atuna/work/maia
+COMPACT=${CODE}/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml
+PFLOW=${CODE}/mlpf_postprocess/particleflow
+
+#
+# setup if necessary
+#
+if ! command -v ddsim >/dev/null 2>&1; then
+    echo "Setting env ${SEED} at $(date) ..."
+    source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/*/mucol*/setup.sh
+    # export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
+fi
+
+#
+# file names
+#
+TYPEEVENT="ttbar"
+GEN_TEMPLATE=p8_mumu_tt_ecm10000.cmd
+GEN_CMD=p8_mumu_tt_ecm10000_${SEED}.cmd
+GEN_HEPMC=${TYPEEVENT}_${SEED}.hepmc
+SIM_STEER=${TOPDIR}/sim_steer_GEN_CONDOR.py
+SIM_SLCIO=${TYPEEVENT}_sim_${SEED}.slcio
+REC_STEER=${TOPDIR}/steer_reco_${TYPEEVENT}.py
+REC_SLCIO=${TYPEEVENT}_reco_${SEED}.slcio
+REC_ROOT=${REC_SLCIO}.edm4hep.root
+REC_PATCH=${TOPDIR}/patch.txt
+GEN_LOG=log_gen_${SEED}.txt
+SIM_LOG=log_sim_${SEED}.txt
+REC_LOG=log_rec_${SEED}.txt
+POST_LOG=log_post_${SEED}.txt
+
+#
+# gen
+#
+if $DO_GEN; then
+    echo "Running gen ${SEED} at $(date) ..."
+    cp ${TOPDIR}/${GEN_TEMPLATE} ${GEN_CMD}
+    sed -i s/12345/${SEED}/g ${GEN_CMD}
+    k4run \
+        ${TOPDIR}/pythia.py \
+        --Dumper.Filename ${GEN_HEPMC} \
+        --Pythia8.PythiaInterface.pythiacard ${GEN_CMD} \
+        &> ${GEN_LOG}
+fi
+
+#
+# sim
+#
+if $DO_SIM; then
+    echo "Running sim ${SEED} at $(date) ..."
+    ddsim \
+        --inputFile ${GEN_HEPMC} \
+        --outputFile ${SIM_SLCIO} \
+        --steeringFile ${SIM_STEER} \
+        --compactFile ${COMPACT} \
+        --numberOfEvents ${EVENTS_PER_JOB} \
+        --random.seed ${SEED} \
+        &> ${SIM_LOG}
+fi
+
+#
+# rec
+#
+if $DO_REC; then
+    echo "Running rec ${SEED} at $(date) ..."
+    k4run ${REC_STEER} \
+         --TypeEvent ${TYPEEVENT} \
+         --InFileName ${SEED} \
+         --code ${CODE} \
+         --skipTrackerConing \
+         &> ${REC_LOG}
+    lcio2edm4hep ${REC_SLCIO} ${REC_ROOT} ${REC_PATCH}
+fi
+
+#
+# postprocess
+#
+if $DO_POST; then
+    echo "Running post-processing ${SEED} at $(date) ..."
+    export PYTHONPATH=${PFLOW}
+    ${TOPDIR}/env/bin/python \
+        ${PFLOW}/mlpf/data/key4hep/postprocessing.py \
+        --input ${REC_ROOT} \
+        --outpath $(pwd) \
+        --detector maia \
+        &> ${POST_LOG}
+fi
+
+#
+# cleanup
+#
+if $DO_CLEANUP; then
+    echo "Running cleanup ${SEED} at $(date) ..."
+    rm -f ${GEN_CMD} ${GEN_HEPMC} ${SIM_SLCIO} ${REC_SLCIO} ${REC_ROOT}
+fi
